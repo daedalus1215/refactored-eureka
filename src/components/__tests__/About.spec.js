5aac892a218198b99e89dee97c1c41086af5cc51
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2024-07-06 16:26:10 -0400

    vitest: mount test

---
+++ src/components/__tests__/About.spec.js
@@ -0,0 +1,15 @@
+import { test } from 'vitest';
+import { mount } from '@vue/test-utils'
+import About from '../About.vue';
+
+describe('About.vue', () => {
+    test('renders inner text', () => {
+        // Arrange
+        const wrapper = mount(About)
+
+        // Act
+
+        // Assert
+        expect(wrapper.text()).toEqual('This is an about page');
+    });
+});
